7802b94684bd98045cafb5b9676cd93a223d3df6
mattr <matt.roberts@thetrainline.com>
2018-05-22 16:13:05 +0100

    Add cors

--- packages/tic-tac-toe-server/src/index.ts
+++ packages/tic-tac-toe-server/src/index.ts
@@ -1,28 +1,40 @@
+import cors from "@koa/cors";
 import { graphiqlKoa, graphqlKoa } from "apollo-server-koa";
 import Koa from "koa";
 import koabody from "koa-bodyparser";
 import Router from "koa-router";
-import { Schema } from './schemas';
+import { Schema } from "./schemas";
 
 const app = new Koa();
 const router = new Router();
+
+app.use(cors());
 
 app.use(koabody());
 router.get("/ping", async ctx => {
     ctx.body = "pong";
 });
 
-router.get('/graphql', graphqlKoa({
-    schema: Schema
-}));
-router.post('/graphql', graphqlKoa({
-    schema: Schema
-}));
+router.get(
+    "/graphql",
+    graphqlKoa({
+        schema: Schema
+    })
+);
+router.post(
+    "/graphql",
+    graphqlKoa({
+        schema: Schema
+    })
+);
 
-router.get('/graphiql', graphiqlKoa({
-    endpointURL: '/graphql'
-}))
+router.get(
+    "/graphiql",
+    graphiqlKoa({
+        endpointURL: "/graphql"
+    })
+);
 
 app.use(router.routes());
 app.listen(3000);
-console.log('listening on 3000');
+console.log("listening on 3000");
