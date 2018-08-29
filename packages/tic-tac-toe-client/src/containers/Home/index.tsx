a32db2f3d939a6418ab170c3ac42a5a5fd645d26
mattcroberts <mattroberts.uk@gmail.com>
2018-08-29 22:39:22 +0100

    Refactor newGame mutation handling

--- packages/tic-tac-toe-client/src/containers/Home/index.tsx
+++ packages/tic-tac-toe-client/src/containers/Home/index.tsx
@@ -1,42 +1,40 @@
+import * as H from "history";
 import * as React from "react";
-import { graphql } from "react-apollo";
+import { Mutation } from "react-apollo";
 import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
+import { Mutation as IMutation } from "../../../typings/types";
 import NEW_GAME from "./newGame.graphql";
-
 interface IProps {
-    newGame: () => undefined;
+    history: H.History;
 }
 
 class Home extends React.Component<IProps> {
     constructor(props: IProps) {
         super(props);
-        this.onClick = this.onClick.bind(this);
+        this.onNewGame = this.onNewGame.bind(this);
     }
-
-    public async onClick() {
-        const { history, newGame }: any = this.props;
-        const {
-            data: { newGame: grid }
-        } = await newGame();
-
-        if (grid.id) {
-            history.push(`/game/${grid.id}`);
-        }
-    }
-
     public render() {
         return (
-            <React.Fragment>
-                <button onClick={this.onClick}>Play</button>
-            </React.Fragment>
+            <Mutation mutation={NEW_GAME} onCompleted={this.onNewGame}>
+                {(newGame, { loading }) => {
+                    const onClick = () => newGame();
+                    return (
+                        <button onClick={onClick} disabled={loading}>
+                            {loading ? "Loading..." : "New Game"}
+                        </button>
+                    );
+                }}
+            </Mutation>
         );
+    }
+
+    private onNewGame({ newGame }: IMutation) {
+        const { history } = this.props;
+        if (newGame.id) {
+            history.push(`/game/${newGame.id}`);
+        }
     }
 }
 
-export default compose(
-    withRouter,
-    graphql(NEW_GAME, {
-        name: "newGame"
-    })
-)(Home);
+export default compose(withRouter)(Home);
--- packages/tic-tac-toe-client/typings/types.ts
+++ packages/tic-tac-toe-client/typings/types.ts
@@ -1,4 +1,32 @@
 /* tslint:disable */
+import { GraphQLResolveInfo } from "graphql";
+
+export type Resolver<Result, Parent = any, Context = any, Args = any> = (
+    parent: Parent,
+    args: Args,
+    context: Context,
+    info: GraphQLResolveInfo
+) => Promise<Result> | Result;
+
+export type SubscriptionResolver<
+    Result,
+    Parent = any,
+    Context = any,
+    Args = any
+> = {
+    subscribe<R = Result, P = Parent>(
+        parent: P,
+        args: Args,
+        context: Context,
+        info: GraphQLResolveInfo
+    ): AsyncIterator<R | Result>;
+    resolve?<R = Result, P = Parent>(
+        parent: P,
+        args: Args,
+        context: Context,
+        info: GraphQLResolveInfo
+    ): R | Result | Promise<R | Result>;
+};
 
 export interface Query {
     grid: Grid;
@@ -20,6 +48,10 @@ export interface GridItem {
 
 export interface Mutation {
     executeTurn: Grid;
+    newGame: Grid;
+}
+export interface GridQueryArgs {
+    id: string;
 }
 export interface ExecuteTurnMutationArgs {
     id: string;
@@ -31,4 +63,105 @@ export interface ExecuteTurnMutationArgs {
 export enum Player {
     NAUGHT = "NAUGHT",
     CROSS = "CROSS"
+}
+
+export namespace QueryResolvers {
+    export interface Resolvers<Context = any> {
+        grid?: GridResolver<Grid, any, Context>;
+    }
+
+    export type GridResolver<R = Grid, Parent = any, Context = any> = Resolver<
+        R,
+        Parent,
+        Context,
+        GridArgs
+    >;
+    export interface GridArgs {
+        id: string;
+    }
+}
+
+export namespace GridResolvers {
+    export interface Resolvers<Context = any> {
+        id?: IdResolver<string, any, Context>;
+        gridItems?: GridItemsResolver<GridItem[][], any, Context>;
+        currentPlayer?: CurrentPlayerResolver<Player, any, Context>;
+        winner?: WinnerResolver<Player | null, any, Context>;
+        isFinished?: IsFinishedResolver<boolean, any, Context>;
+        size?: SizeResolver<number, any, Context>;
+    }
+
+    export type IdResolver<R = string, Parent = any, Context = any> = Resolver<
+        R,
+        Parent,
+        Context
+    >;
+    export type GridItemsResolver<
+        R = GridItem[][],
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+    export type CurrentPlayerResolver<
+        R = Player,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+    export type WinnerResolver<
+        R = Player | null,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+    export type IsFinishedResolver<
+        R = boolean,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+    export type SizeResolver<
+        R = number,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+}
+
+export namespace GridItemResolvers {
+    export interface Resolvers<Context = any> {
+        id?: IdResolver<string, any, Context>;
+        player?: PlayerResolver<Player | null, any, Context>;
+    }
+
+    export type IdResolver<R = string, Parent = any, Context = any> = Resolver<
+        R,
+        Parent,
+        Context
+    >;
+    export type PlayerResolver<
+        R = Player | null,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
+}
+
+export namespace MutationResolvers {
+    export interface Resolvers<Context = any> {
+        executeTurn?: ExecuteTurnResolver<Grid, any, Context>;
+        newGame?: NewGameResolver<Grid, any, Context>;
+    }
+
+    export type ExecuteTurnResolver<
+        R = Grid,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context, ExecuteTurnArgs>;
+    export interface ExecuteTurnArgs {
+        id: string;
+        player: string;
+        x: number;
+        y: number;
+    }
+
+    export type NewGameResolver<
+        R = Grid,
+        Parent = any,
+        Context = any
+    > = Resolver<R, Parent, Context>;
 }
