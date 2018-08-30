dbc15f13ffec8181659b8ee851e2735671982da0
mattcroberts <mattroberts.uk@gmail.com>
2018-08-30 23:33:33 +0100

    Convert ribbon to component for parceljs

--- packages/tic-tac-toe-client/src/index.tsx
+++ packages/tic-tac-toe-client/src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
+import GithubRibbon from "./components/GithubRibbon";
 import App from "./containers/App";
 import "./index.css";
 
+ReactDOM.render(<GithubRibbon />, document.getElementById(
+    "ribbon"
+) as HTMLElement);
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
