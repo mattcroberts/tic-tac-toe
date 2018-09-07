25ebfcb58c383ff9a6c3ba8d4c9b9b140a6a1b27
mattr <matt.roberts@thetrainline.com>
2018-09-07 14:40:43 +0100

    Enable server code coverage

--- packages/tic-tac-toe-server/jest.config.js
+++ packages/tic-tac-toe-server/jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+    roots: ["<rootDir>/src"],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    }
+};
