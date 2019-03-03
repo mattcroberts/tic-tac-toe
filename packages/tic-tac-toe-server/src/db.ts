4b3e1ee4563f018596ffb3deb4975f03a5f82578
mattcroberts <mattroberts.uk@gmail.com>
2019-03-03 21:26:59 +0000

    Fix production build

--- packages/tic-tac-toe-server/src/db.ts
+++ packages/tic-tac-toe-server/src/db.ts
@@ -2,15 +2,26 @@ import "reflect-metadata";
 import { createConnection, getConnectionOptions } from "typeorm";
 
 export const connectWithRetry = async (retries = 5, timeout = 500) => {
-    const connectionOptions = Object.assign(await getConnectionOptions(), {
-        host: process.env.TYPEORM_HOST,
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE
-    });
+    const connectionOptions = Object.assign(
+        await getConnectionOptions(),
+        {
+            host: process.env.TYPEORM_HOST,
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE
+        },
+        process.env.NODE_ENV === "production"
+            ? {
+                  entities: [process.env.TYPEORM_ENTITIES_DIR],
+                  subscribers: [process.env.TYPEORM_SUBSCRIBERS_DIR],
+                  migrations: [process.env.TYPEORM_MIGRATIONS_DIR]
+              }
+            : {}
+    );
 
     try {
         await createConnection(connectionOptions);
+        console.log("DB Connected");
     } catch (e) {
         console.error(e);
 
@@ -26,6 +37,4 @@ export const connectWithRetry = async (retries = 5, timeout = 500) => {
             return;
         }
     }
-
-    console.log("DB Connected");
 };
