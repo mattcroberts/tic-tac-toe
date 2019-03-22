a40366643b843a3ac99f1870fd477d650848a262
Matt Roberts <matt.roberts@sapient.com>
2019-03-22 17:14:10 +0000

    use commit hash for docker image version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+export API_URI=/tictactoe/graphql
+export TAG=latest
+source ./scripts/env.sh
+
 docker volume create volumerize-credentials
 docker volume create volumerize-cache
 docker volume create pg-data
+
+if [ "$VOLUMERIZE_TARGET" && "$GOOGLE_DRIVE_ID" && "$GOOGLE_DRIVE_SECRET" ]; then
+    docker run -it --rm \
+        -v "volumerize-cache:/volumerize-cache" \
+        -v "volumerize-credentials:/credentials" \
+        -v "pg-data:/source" \
+        -e "VOLUMERIZE_SOURCE=/source" \
+        -e "VOLUMERIZE_TARGET=$VOLUMERIZE_TARGET" \
+        -e "GOOGLE_DRIVE_ID=$GOOGLE_DRIVE_ID" \
+        -e "GOOGLE_DRIVE_SECRET=$GOOGLE_DRIVE_SECRET" \
+        blacklabelops/volumerize restore
+else
+    echo "skipping DB restore"
+fi
+
+docker-compose -p ttt -f docker-compose.do.yml up -d
