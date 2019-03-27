0ba2f99df38c6cfa73b7ccb809e4a0494d2e897c
Matt Roberts <matt.roberts@sapient.com>
2019-03-27 12:13:05 +0000

    backup/restore tweaks

--- scripts/volumerize/backup-db.sh
+++ scripts/volumerize/backup-db.sh
@@ -4,4 +4,4 @@ echo "Creating $VOLUMERIZE_SOURCE folder if not exists"
 mkdir -p $VOLUMERIZE_SOURCE
 
 echo "Creating PG dump"
-PGPASSWORD=${POSTGRES_PASSWORD} pg_dump -U ${POSTGRES_USERNAME} -h postgres ${DATABASE_NAME} > ${VOLUMERIZE_SOURCE}/backup.sql
+PGPASSWORD=${POSTGRES_PASSWORD} pg_dump -U ${POSTGRES_USERNAME} -h postgres ${DATABASE} > ${VOLUMERIZE_SOURCE}/backup.sql
--- scripts/volumerize/restore-db.sh
+++ scripts/volumerize/restore-db.sh
@@ -1,3 +1,7 @@
 apk add --no-cache postgresql-client
 
-PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USERNAME} -h postgres ${DATABASE_NAME} -1 -f ${VOLUMERIZE_SOURCE}/backup.sql
+export PGPASSWORD=${POSTGRES_PASSWORD}
+
+psql -U ${POSTGRES_USERNAME} -h postgres postgres -c "DROP DATABASE ${DATABASE};"
+psql -U ${POSTGRES_USERNAME} -h postgres postgres -c "CREATE DATABASE ${DATABASE};"
+psql -U ${POSTGRES_USERNAME} -h postgres ${DATABASE} -1 -f ${VOLUMERIZE_SOURCE}/backup.sql
